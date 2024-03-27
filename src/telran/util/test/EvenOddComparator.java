818bb80f24c5669a5f45a9ff228d830a37a25de0
ElenaNisnevich <e488764@gmail.com>
2024-03-27 16:38:08 +0200

    BubbleSort and EvenOddComparator

---
+++ src/telran/util/test/EvenOddComparator.java
@@ -0,0 +1,32 @@
+package telran.util.test;
+
+import java.util.Comparator;
+
+public class EvenOddComparator implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		int res=1;
+//if(o1%2==0&&o2%2==0)res=o1.compareTo(o2);
+//if (o1%2!=0&&o2%2!=0) res=(o1.compareTo(o2))*(-1);	
+//if(o1%2==0&&o2%2!=0)res=-1;
+//return res;
+		
+//		if((o1%2==0&&o2%2==0)||(o1%2!=0&&o2%2!=0)) 
+//		{ res=o1.compareTo(o2);
+//	      if (o1%2!=0) res=-res;}	
+//	if(o1%2==0&&o2%2!=0)res=-1;
+//	return res;	
+	
+	if((o1%2==0&&o2%2==0)||(o1%2!=0&&o2%2!=0)) 
+	  { res=o1.compareTo(o2);
+        if (o1%2!=0) res=-res;}
+	else 
+	 {if(o1%2==0)res=-1;}
+return res;	
+}
+}
+
+
+ 
+ 
